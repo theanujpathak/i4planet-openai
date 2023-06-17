d0a0798bd08359f1972025bf6bdceb48d2e0c1f8
Anuj Pathak <pathak.anuj74@gmail.com>
2023-06-17 09:16:17 +0530

    Security Update 2

--- app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or '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'
--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or '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'
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
